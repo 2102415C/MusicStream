878f4ba3ce51f4eb37f340b3d286ba1bedf86907
Music app <2102415C@student.tp.edu.sg>
2021-05-24 12:11:06 +0800

    v1.5

--- app/src/main/java/com/example/musicstream/MainActivity.java
+++ app/src/main/java/com/example/musicstream/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.musicstream;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void handleSelection(View myView)
+    {
+        String resourceId=getResources().getResourceEntryName(myView.getId());
+        Log.d("temasek","The is of the pressed ImageButton is: "+resourceId);
     }
 }
