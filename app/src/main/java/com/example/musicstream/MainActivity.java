f777ae17f2a9aaff68a22113162cc136528ab76e
Music app <2102415C@student.tp.edu.sg>
2021-06-01 18:05:40 +0800

    v1.7

--- app/src/main/java/com/example/musicstream/MainActivity.java
+++ app/src/main/java/com/example/musicstream/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.musicstream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    SongCollection songCollection = new SongCollection();
+    //I am creating a variable/copy of the SongCollection class.
+    //So that I am able to call the methods and variables from the SongCollection class, by accessing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
     {
         String resourceId=getResources().getResourceEntryName(myView.getId());
         Log.d("temasek","The is of the pressed ImageButton is: "+resourceId);
+        int currentArrayIndex = songCollection.searchSongById(resourceId);
+        sendDataToActivity(currentArrayIndex);
+    }
+    public void sendDataToActivity(int index)
+    {
+        Intent intent = new Intent(this, PlaySongActivity.class);
+        intent.putExtra("index",index);//optional
+        startActivity(intent);
     }
 }
