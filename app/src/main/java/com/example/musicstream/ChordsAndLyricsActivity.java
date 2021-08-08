8c51d90e358d6de6f1518abcc1f21336b7d896d3
Music app <2102415C@student.tp.edu.sg>
2021-08-08 21:14:36 +0800

    v4.0

--- app/src/main/java/com/example/musicstream/ChordsAndLyricsActivity.java
+++ app/src/main/java/com/example/musicstream/ChordsAndLyricsActivity.java
@@ -11,11 +11,12 @@ import android.widget.TextView;
 public class ChordsAndLyricsActivity extends AppCompatActivity {
     String chordsAndLyrics = "";
     SongCollection songCollection = new SongCollection();
+    int currentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chords_and_lyrics);
-
+        displayChordsAndLyricsBasedOnIndex(currentIndex);
     }
     public void displayChordsAndLyricsBasedOnIndex(int index)
     {
@@ -23,6 +24,10 @@ public class ChordsAndLyricsActivity extends AppCompatActivity {
         chordsAndLyrics = song.getChordsAndLyrics();
         TextView txtChordsAndLyrics = findViewById(R.id.txtChordsAndLyrics);
         txtChordsAndLyrics.setText(chordsAndLyrics);
+    }
+    public void displayChordsAndLyrics(View view){
+        currentIndex=songCollection.getNextSong(currentIndex);
+        displayChordsAndLyricsBasedOnIndex(currentIndex);
     }
     public void goBack(View view){
         Intent intent = new Intent(this,PlaySongActivity.class);
